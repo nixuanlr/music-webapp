8f1a30ac6b3cb3512b016c4653a92940332f1e5b
liran_nixuan <1069391874@qq.com>
2018-11-10 18:12:45 +0800

    update --添加 favicon.ico --添加 component aside --添加 component footer --添加 component header

--- web/src/app/com/com.module.ts
+++ web/src/app/com/com.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, AsideComponent, FooterComponent],
   imports: [
     CommonModule
   ]
--- web/src/app/pages/index.module.ts
+++ web/src/app/pages/index.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
 import { FindMusicComponent } from './find-music/find-music.component';
+import { IndexRoutingModule } from './index-routing.module';
 
 @NgModule({
   declarations: [IndexComponent, FindMusicComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IndexRoutingModule
   ]
 })
 export class IndexModule { }
